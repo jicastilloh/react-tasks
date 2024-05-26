bbb4e74618cc1c1619a8782af69c21f2fd8986f0
Junior Castillo <castillojunior099@gmail.com>
2024-05-26 00:18:26 -0600

    Finalizado

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/react-tasks-application/',  // Asegúrate de que esto coincide con el subdirectorio de tu aplicación
-  plugins: [react()],
-  build: {
-    outDir: 'dist',  // o el directorio que estés utilizando para la construcción
-  },
+  plugins: [react()]
 });
